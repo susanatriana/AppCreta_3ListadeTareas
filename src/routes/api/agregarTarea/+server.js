577075005dcdeed8f2a904315edb51bc924a65fb
susanatriana <susanatrianamoreno@gmil.com>
2024-10-17 20:19:11 -0600

    New endpoint

--- src/routes/api/agregarTarea/+server.js
+++ src/routes/api/agregarTarea/+server.js
@@ -1,23 +1,13 @@
-import pkg from 'pg'
-let { Pool } = pkg
-
-let pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    password: '5860',
-    port: 5432
-})
-
 export async function POST({request}) {
-    let dataFront = await request.json()
-    
-    let client = await pool.connect()
-    let insertQuery = `INSERT INTO tareas(nombre_tarea, completado) VALUES($1, $2)`
-    let values = [dataFront.nombre_tarea, dataFront.completado]
+    let frontData = await request.json()
 
-    let result = await client.query(insertQuery, values)
-    client.release()
+    let response = await fetch("http://13.58.249.92:8000/agregarTarea", {
+        method: 'POST',
+        body: JSON.stringify(frontData),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
 
     return new Response('Todo bien', {status: 200})
 }
--- src/routes/api/obtenerDatos/+server.js
+++ src/routes/api/obtenerDatos/+server.js
@@ -1,19 +1,9 @@
-import pkg from 'pg'
-const { Pool } = pkg
-
-const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    password: '5860',
-    port: 5432
-})
-
 export async function GET() {
-    let client = await pool.connect()
-    let query = 'SELECT * FROM tareas'
-    let result = await client.query(query)
-    console.log(result.rows)
 
-    return new Response(JSON.stringify(result.rows), {status: 200})
+    let response = await fetch('http://13.58.249.92:8000/obtenerDatos')
+    let datos = await response.json()
+
+    console.log(datos)
+
+    return new Response(JSON.stringify(datos),{status: 200})
 }
